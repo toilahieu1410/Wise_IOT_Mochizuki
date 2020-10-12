ce1968771c9ead27e7706c169aa1041d616fbc25
duongtien512 <duongtien512@gmail.com>
2020-10-12 16:26:48 +0700

    Initial commit

---
+++ android/app/src/main/java/com/wise_iot_mochizuki/MainActivity.java
@@ -0,0 +1,15 @@
+package com.wise_iot_mochizuki;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "Wise_Iot_Mochizuki";
+  }
+}
